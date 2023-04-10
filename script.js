1900613444ffe66712f4fc24be604bad01f25fff
Mohamed Mudassir <122519960+muthassir@users.noreply.github.com>
2023-04-10 14:43:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let input = document.getElementById("inp");
+let result = document.getElementById("res");
+let winners = document.getElementById("win");
+let vanish = document.getElementById("show")
+
+function show(num){
+    input.innerHTML += num
+}
+
+function get(){
+    let shows = Math.floor(Math.random()*22)
+    console.log(shows);
+    if (shows == 0) {
+        shows = shows + 1
+    }
+    result.innerHTML = shows
+    if (input.innerHTML == "") {
+        alert("select the number")
+        result.innerHTML = ""
+        
+    }
+    if(input.innerHTML >= 22){
+      alert("select number correctly")
+      reset()
+    }
+    
+      if (input.innerHTML === result.innerHTML && input.innerHTML != "") {
+        winners.style.display="block"
+        document.getElementById("bgm").play()
+      }
+      vanish.style.display="none"
+}
+
+function reset(){
+    input.innerHTML = ""
+    result.innerHTML = ""
+    vanish.style.display="block"
+}
+
+function winner() {
+    winners.style.display="none"
+    
+}
